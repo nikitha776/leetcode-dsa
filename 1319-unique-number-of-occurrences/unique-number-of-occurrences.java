3e91c9219a2feb00ddd9a3a70a8fa5bb4de6fffb
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2024-11-14 10:30:38 +0530

    Time: 2 ms (98.93%) | Memory: 41.8 MB (46.41%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,24 +1,11 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        int count;
-        boolean[] x = new boolean[1001];  
-        Arrays.sort(arr);
-
-        for (int i = 0; i < arr.length; i++) {
-            if (i > 0 && arr[i] == arr[i - 1]) {
-                continue;
-            }
-            count = 0;
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    count++;
-                }
-            }
-            if (x[count]) {
-                return false;  
-            }
-            x[count] = true;  
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i < arr.length;i++) {
+            if(!map.containsKey(arr[i])) map.put(arr[i],1);
+            else map.put(arr[i],map.get(arr[i])+1);
         }
-        return true;  
+        HashSet<Integer> set = new HashSet<>(map.values());
+        return map.size() == set.size();
     }
 }
