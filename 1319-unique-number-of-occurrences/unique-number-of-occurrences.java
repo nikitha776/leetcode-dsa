3ebf0dde76c51002ab592b86666d14f920d17f77
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2024-11-14 10:18:32 +0530

    Time: 3 ms (30.28%) | Memory: 41.4 MB (91.96%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -1,47 +1,24 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        // int count = 0;
-        // int prevCount = 0;
-        // Arrays.sort(arr);
-        // for(int i = 0;i < arr.length;i++) {
-        //     count  = 0;
-        //     if(i > 0 && arr[i] == arr[i-1]) {
-        //         continue;
-        //     }
-        //     for(int j = 0; j < arr.length;j++) {
-        //         if(arr[i] == arr[j]) {
-        //             count++;
-        //         }
-        //     }
-        //     if(count == prevCount) return false;
-        //     else prevCount  = count;
-        // }
-        // return true;
         int count;
-        boolean[] seenCounts = new boolean[1001];  // Track counts encountered (assuming max count <= 1000)
+        boolean[] x = new boolean[1001];  
         Arrays.sort(arr);
 
         for (int i = 0; i < arr.length; i++) {
-            // Skip duplicate elements
             if (i > 0 && arr[i] == arr[i - 1]) {
                 continue;
             }
-            
-            // Count occurrences of arr[i]
             count = 0;
             for (int j = 0; j < arr.length; j++) {
                 if (arr[i] == arr[j]) {
                     count++;
                 }
             }
-            
-            // Check if this count has already been encountered
-            if (seenCounts[count]) {
-                return false;  // Duplicate count found
+            if (x[count]) {
+                return false;  
             }
-            seenCounts[count] = true;  // Mark this count as encountered
+            x[count] = true;  
         }
-        
-        return true;  // All counts are unique
+        return true;  
     }
 }
