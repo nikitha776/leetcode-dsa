6dde81d7fdf902daa332ddbf6e17719f1ee0f1a0
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2024-11-27 09:33:55 +0530

    Time: 25 ms (5.04%) | Memory: 44.8 MB (27.05%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,14 +1,49 @@
-class Solution {
-    public int removeDuplicates(int[] arr) {
-        int j = 1;
-        if(arr.length == 1) {
-            return 1;
-        }
-        for(int i = 1;i < arr.length;i++) {
-            if(arr[i] != arr[i-1]) {
-                arr[j++] = arr[i]; 
+ class Solution {
+     public static int partition(int a[],int low,int high)
+     {
+        int i=low-1;
+        int pivot=a[high];
+        int j;
+        for(j=low;j<high;j++)
+        {
+            if(a[j]<pivot)
+            {i++;
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
             }
         }
-        return j;
+        i++;
+        int temp=a[i];
+        a[i]=pivot;
+        a[high]=temp;
+        return i;
+     }
+      public static void quicksort(int a[],int low,int high)
+      {
+        if(low<high)
+        {
+            int loc=partition(a,low,high);
+            quicksort(a,low,loc-1);
+            quicksort(a,loc+1,high);
+        }
+      }
+    public int removeDuplicates(int[] nums) {
+        quicksort(nums,0,nums.length-1);
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(!list.contains(nums[i]))
+            {
+                list.add(nums[i]);
+            }
+        }
+
+        int[] a=new int[list.size()];
+        for(int i=0;i<list.size();i++)
+        {
+            nums[i]=list.get(i);
+        }
+        return list.size();
     }
 }
