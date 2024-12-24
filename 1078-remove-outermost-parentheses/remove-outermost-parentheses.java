1be700bbdaee5db2d08f4e9d3ce149bb1ba7441e
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2024-12-24 11:24:41 +0530

    Time: 15 ms (12.02%) | Memory: 45.3 MB (9.27%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,18 +1,35 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        Stack<Character> st = new Stack<>();
+        // Stack<Character> st = new Stack<>();
         String ans = "";
-        for(int i = 0;i < s.length();i++) {
-            if(s.charAt(i) == '(') {
-                if(!st.isEmpty()) {
-                    ans = ans + s.charAt(i);
+        // for(int i = 0;i < s.length();i++) {
+        //     if(s.charAt(i) == '(') {
+        //         if(!st.isEmpty()) {
+        //             ans = ans + s.charAt(i);
+        //         }
+        //         st.push(s.charAt(i));
+        //     }   
+        //     if(s.charAt(i) == ')') {
+        //         st.pop();
+        //         if(!st.isEmpty()) {
+        //             ans = ans + s.charAt(i);
+        //         }
+        //     }
+        // }
+        // return ans;
+        int count = 0;
+        for(char j = 0;j < s.length();j++) {
+            char i = s.charAt(j);
+            if(i == '(') {
+                if(count > 0) {
+                    ans = ans + i;
                 }
-                st.push(s.charAt(i));
-            }   
-            if(s.charAt(i) == ')') {
-                st.pop();
-                if(!st.isEmpty()) {
-                    ans = ans + s.charAt(i);
+                count++;
+            }
+            if(i == ')') {
+                count--;
+                if(count > 0) {
+                    ans = ans + i;
                 }
             }
         }
