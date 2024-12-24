6a342f7d2a8b6702d2872ac9fee824a3828f0b4d
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2024-12-24 14:56:56 +0530

    Time: 2 ms (99.12%) | Memory: 43.2 MB (57.50%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,24 +1,17 @@
- class Solution {
+class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-        	return true;
-        }
-        int start = 0;
-        int last = s.length() - 1;
-        while(start <= last) {
-        	char currFirst = s.charAt(start);
-        	char currLast = s.charAt(last);
-        	if (!Character.isLetterOrDigit(currFirst )) {
-        		start++;
-        	} else if(!Character.isLetterOrDigit(currLast)) {
-        		last--;
-        	} else {
-        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        			return false;
-        		}
-        		start++;
-        		last--;
-        	}
+        int b = 0;
+        int e = s.length() - 1;
+        while(b <= e) {
+            char i = s.charAt(b);
+            char j = s.charAt(e);
+            if(!Character.isLetterOrDigit(i)) b++;
+            else if(!Character.isLetterOrDigit(j)) e--;
+            else {
+                if(Character.toLowerCase(i) != Character.toLowerCase(j)) return false;
+                b++;
+                e--;
+            }
         }
         return true;
     }
