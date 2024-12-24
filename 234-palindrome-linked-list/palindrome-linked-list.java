e4735e3e304fd3db3af9f1d94b200a366ab600d3
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2024-12-24 14:49:09 +0530

    Time: 4 ms (83.32%) | Memory: 68.2 MB (52.63%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,17 +9,44 @@
  * }
  */
 class Solution {
+    public ListNode reverse(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = curr;
+        while(curr !=  null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> st = new Stack<>();
-        ListNode temp = head;
-        while(temp != null) {
-            st.push(temp.val);
-            temp = temp.next;
+        // Stack<Integer> st = new Stack<>();
+        // ListNode temp = head;
+        // while(temp != null) {
+        //     st.push(temp.val);
+        //     temp = temp.next;
+        // }
+        // temp = head;
+        // while(temp != null && st.pop() == temp.val) {
+        //     temp = temp.next;
+        // }
+        // return temp == null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        temp = head;
-        while(temp != null && st.pop() == temp.val) {
-            temp = temp.next;
+        ListNode rev = reverse(slow);
+        while(rev != null) {
+            if(rev.val != head.val) {
+                return false;
+            }
+            rev = rev.next;
+            head = head.next;
         }
-        return temp == null;
+        return true;
     }
 }
