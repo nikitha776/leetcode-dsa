27ef118d62415927eda60b3b348337141c868409
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2024-12-27 12:45:06 +0530

    Time: 6 ms (85.97%) | Memory: 42.7 MB (91.11%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -21,33 +21,3 @@ class Solution {
     }
 }
 
-// class Solution {
-//     public String reverseWords(String s) {
-//         StringBuilder sb = new StringBuilder();
-//         StringBuilder temp = new StringBuilder();
-        
-//         for (int i = s.length() - 1; i >= 0; i--) {
-//             char ch = s.charAt(i);
-            
-//             if (ch != ' ') {
-//                 temp.append(ch); // Collect characters for the current word
-//             } else if (temp.length() > 0) { // Append the word if ready
-//                 if (sb.length() > 0) {
-//                     sb.append(" "); // Add space between words
-//                 }
-//                 sb.append(temp.reverse()); // Reverse and append the word
-//                 temp.setLength(0); // Clear temp for the next word
-//             }
-//         }
-        
-//         // Append the last word if there's no trailing space
-//         if (temp.length() > 0) {
-//             if (sb.length() > 0) {
-//                 sb.append(" ");
-//             }
-//             sb.append(temp.reverse());
-//         }
-        
-//         return sb.toString();
-//     }
-// }
