a32de77cb934435b7c45675e8ebd795d6b59f975
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-01-08 15:13:35 +0530

    Time: 2 ms (86.88%) | Memory: 48.2 MB (62.61%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -9,25 +9,57 @@
  * }
  */
 class Solution {
+    public ListNode reverse(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
     public void reorderList(ListNode head) {
-        Stack<Integer> st = new Stack<>();
+        // Stack<Integer> st = new Stack<>();
         ListNode temp = head;
         int n = 0;
         while(temp != null) {
-            st.push(temp.val);
+            // st.push(temp.val);
             n++;
             temp = temp.next;
         }
-        temp = head;
+        // temp = head;
         ListNode x = head;
-        for(int i = 0;i < n/2;i++) {
-            ListNode newNode = new ListNode(st.pop());
-            newNode.next = temp.next;
-            temp.next = newNode;
-            x = temp.next;
-            temp = temp.next.next;
+        // for(int i = 0;i < n/2;i++) {
+        //     ListNode newNode = new ListNode(st.pop());
+        //     newNode.next = temp.next;
+        //     temp.next = newNode;
+        //     x = temp.next;
+        //     temp = temp.next.next;
+        // }
+        // if(n % 2 == 0) x.next = null;
+        // else temp.next = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        if(n % 2 == 0) x.next = null;
-        else temp.next = null;
+        ListNode head1 = head;
+        ListNode head2 = head;
+        if(n % 2 == 0) head2 = slow;
+        else head2 = slow.next;
+        ListNode newHead = reverse(head2);
+        ListNode tem = head1;
+        while(newHead != null) {
+            x = newHead.next;
+            newHead.next = tem.next;
+            tem.next = newHead;
+            tem = tem.next.next;
+            newHead = x;
+        }
+        slow.next = null;
     }
 }
