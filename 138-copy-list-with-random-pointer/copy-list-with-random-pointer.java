79f8a3b8ae13f488fca8d7a0b6c07e678466e6b1
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-01-08 23:04:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (85.88%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,19 +15,43 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        HashMap<Node,Node> map = new HashMap<>();
+        // HashMap<Node,Node> map = new HashMap<>();
+        // Node temp = head;
+        // while(temp != null) {
+        //     map.put(temp,new Node(temp.val));
+        //     temp = temp.next;
+        // }
+        // temp = head;
+        // while(temp != null) {
+        //     Node copyNode = map.get(temp);
+        //     copyNode.next = map.get(temp.next);
+        //     copyNode.random = map.get(temp.random);
+        //     temp = temp.next;
+        // }
+        // return map.get(head);
+
         Node temp = head;
         while(temp != null) {
-            map.put(temp,new Node(temp.val));
-            temp = temp.next;
+            Node newNode = new Node(temp.val);
+            newNode.next = temp.next;
+            temp.next = newNode;
+            temp = temp.next.next;
         }
         temp = head;
         while(temp != null) {
-            Node copyNode = map.get(temp);
-            copyNode.next = map.get(temp.next);
-            copyNode.random = map.get(temp.random);
-            temp = temp.next;
+            if(temp.random == null) temp.next.random = null;
+            else temp.next.random = temp.random.next;
+            temp = temp.next.next;
         }
-        return map.get(head);
+        Node dummy = new Node(-1);
+        Node res = dummy;
+        temp = head;
+        while(temp != null) {
+            res.next = temp.next;
+            temp.next = temp.next.next;
+            res = res.next;
+            temp = res.next;
+        }
+        return dummy.next;
     }
 }
