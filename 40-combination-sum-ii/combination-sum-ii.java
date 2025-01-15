dd7a9ff6a2c65b91030c20bff5ebe0d4013e0a8f
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-01-15 16:17:47 +0530

    Time: 3 ms (77.64%) | Memory: 43.9 MB (5.76%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,28 +1,42 @@
 class Solution {
-    public void helper(int[] nums,List<List<Integer>> lists,List<Integer> list,int sum,int target,int idx) {
-        if(sum == target) {
+    // public void helper(int[] nums,List<List<Integer>> lists,List<Integer> list,int sum,int target,int idx) {
+    //     if(sum == target) {
+    //         lists.add(new ArrayList<>(list));
+    //         return;
+    //     }
+    //     if(idx == nums.length || sum > target) {
+    //         return;
+    //     }
+    //     list.add(nums[idx]);
+    //     sum = sum + nums[idx];
+    //     helper(nums,lists,list,sum,target,idx+1);
+    //     while(idx+1 < nums.length && nums[idx] == nums[idx+1]) {
+    //         idx++;
+    //     }
+    //     list.remove(list.size()-1);
+    //     sum = sum - nums[idx];
+    //     helper(nums,lists,list,sum,target,idx+1);
+    //     return;
+    // }
+    public void helper(int[] nums,List<Integer> list,List<List<Integer>> lists,int target,int idx) {
+        if(target == 0) {
             lists.add(new ArrayList<>(list));
             return;
         }
-        if(idx == nums.length || sum > target) {
-            return;
+        for(int i = idx;i < nums.length;i++) {
+            if(i > idx && nums[i] == nums[i-1]) continue;
+            if(target < nums[i]) break;
+            list.add(nums[i]);
+            helper(nums,list,lists,target-nums[i],i+1);
+            list.remove(list.size()-1);
         }
-        list.add(nums[idx]);
-        sum = sum + nums[idx];
-        helper(nums,lists,list,sum,target,idx+1);
-        while(idx+1 < nums.length && nums[idx] == nums[idx+1]) {
-            idx++;
-        }
-        list.remove(list.size()-1);
-        sum = sum - nums[idx];
-        helper(nums,lists,list,sum,target,idx+1);
-        return;
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> lists = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        helper(candidates,lists,list,0,target,0);
+        // helper(candidates,lists,list,0,target,0);
+        helper(candidates,list,lists,target,0);
         return lists;
     }
 }
