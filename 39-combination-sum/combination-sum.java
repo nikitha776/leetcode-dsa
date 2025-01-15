7f7195bdc8b5c1ef6bc453a5ac0cf6c920480348
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-01-15 14:43:45 +0530

    Time: 1 ms (99.83%) | Memory: 45.3 MB (7.12%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,37 @@
 class Solution {
-    public void helper(List<List<Integer>> lists,int[] nums,List<Integer> list,int sum,int target,int idx) {
-        if(sum == target) {
-           lists.add(new ArrayList<>(list));
-           return;
+    // public void helper(List<List<Integer>> lists,int[] nums,List<Integer> list,int sum,int target,int idx) {
+    //     if(sum == target) {
+    //        lists.add(new ArrayList<>(list));
+    //        return;
+    //     }
+    //     if(sum > target || idx == nums.length);
+    //     list.add(nums[idx]);
+    //     sum = sum + nums[idx];
+    //     helper(lists,nums,list,sum,target,idx);
+    //     list.remove(list.size()-1);
+    //     sum = sum - nums[idx];
+    //     helper(lists,nums,list,sum,target,idx+1);
+    //     return;
+    // }
+    public void helper(List<List<Integer>> lists,int[] nums,List<Integer> list,int target,int idx) {
+        if(idx == nums.length) {
+            if(target == 0) {
+                lists.add(new ArrayList<>(list));
+            }
+            return;
         }
-        if(sum > target || idx == nums.length) return;
-        list.add(nums[idx]);
-        sum = sum + nums[idx];
-        helper(lists,nums,list,sum,target,idx);
-        list.remove(list.size()-1);
-        sum = sum - nums[idx];
-        helper(lists,nums,list,sum,target,idx+1);
-        return;
+        if(nums[idx] <= target) {
+            list.add(nums[idx]);
+            helper(lists,nums,list,target-nums[idx],idx);
+            list.remove(list.size()-1);
+        }
+        helper(lists,nums,list,target,idx+1);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> lists = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        helper(lists,candidates,list,0,target,0);
+        // helper(lists,candidates,list,0,target,0);
+        helper(lists,candidates,list,target,0);
         return lists;
     }
 }
