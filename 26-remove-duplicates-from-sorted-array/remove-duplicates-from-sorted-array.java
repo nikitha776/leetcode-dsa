4365a5cdd685a22c6df7ab661d4c7ecf03430be4
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-01-18 19:29:54 +0530

    Time: 1 ms (65.15%) | Memory: 44.7 MB (73.09%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,49 +1,11 @@
- class Solution {
-     public static int partition(int a[],int low,int high)
-     {
-        int i=low-1;
-        int pivot=a[high];
-        int j;
-        for(j=low;j<high;j++)
-        {
-            if(a[j]<pivot)
-            {i++;
-                int temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
+class Solution {
+    public int removeDuplicates(int[] arr) {
+        int j = 1;
+        for(int i = 1;i < arr.length;i++) {
+            if(arr[i] != arr[i-1]) {
+                arr[j++] = arr[i];
             }
         }
-        i++;
-        int temp=a[i];
-        a[i]=pivot;
-        a[high]=temp;
-        return i;
-     }
-      public static void quicksort(int a[],int low,int high)
-      {
-        if(low<high)
-        {
-            int loc=partition(a,low,high);
-            quicksort(a,low,loc-1);
-            quicksort(a,loc+1,high);
-        }
-      }
-    public int removeDuplicates(int[] nums) {
-        quicksort(nums,0,nums.length-1);
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int i=0;i<nums.length;i++)
-        {
-            if(!list.contains(nums[i]))
-            {
-                list.add(nums[i]);
-            }
-        }
-
-        int[] a=new int[list.size()];
-        for(int i=0;i<list.size();i++)
-        {
-            nums[i]=list.get(i);
-        }
-        return list.size();
+        return j;
     }
 }
