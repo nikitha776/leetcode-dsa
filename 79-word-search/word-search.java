2ff9de51cd275d7c082713e11ce1e1b4b5af6b19
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-01-22 22:52:29 +0530

    Time: 153 ms (42.78%) | Memory: 41.8 MB (37.30%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,32 +1,43 @@
 class Solution {
+    // public boolean helper(boolean[][] vis,char[][] board,String word,int i,int j,int idx) {
+    //     if(idx == word.length()-1) {
+    //         return true;
+    //     }
+    //     vis[i][j] = true;
+    //     int r = board.length;
+    //     int c = board[0].length;
+    //     if(i+1 < r && board[i+1][j] == word.charAt(idx+1) && !vis[i+1][j] && helper(vis,board,word,i+1,j,idx+1)) {
+    //         return true;
+    //     }
+    //     if(i-1 >= 0 && board[i-1][j] == word.charAt(idx+1) && !vis[i-1][j] && helper(vis,board,word,i-1,j,idx+1)) {
+    //         return true;
+    //     }
+    //     if(j-1 >= 0 && board[i][j-1] == word.charAt(idx+1) && !vis[i][j-1] && helper(vis,board,word,i,j-1,idx+1)) {
+    //         return true;
+    //     }
+    //     if(j+1 < c && board[i][j+1] == word.charAt(idx+1) && !vis[i][j+1] && helper(vis,board,word,i,j+1,idx+1)) {
+    //         return true;
+    //     }
+    //     vis[i][j] = false;
+    //     return false;
+    // }
     public boolean helper(boolean[][] vis,char[][] board,String word,int i,int j,int idx) {
-        if(idx == word.length()-1) {
-            return true;
-        }
+        if(idx == word.length()) return true;
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(idx) || vis[i][j]) return false;
         vis[i][j] = true;
-        int r = board.length;
-        int c = board[0].length;
-        if(i+1 < r && board[i+1][j] == word.charAt(idx+1) && !vis[i+1][j] && helper(vis,board,word,i+1,j,idx+1)) {
-            return true;
-        }
-        if(i-1 >= 0 && board[i-1][j] == word.charAt(idx+1) && !vis[i-1][j] && helper(vis,board,word,i-1,j,idx+1)) {
-            return true;
-        }
-        if(j-1 >= 0 && board[i][j-1] == word.charAt(idx+1) && !vis[i][j-1] && helper(vis,board,word,i,j-1,idx+1)) {
-            return true;
-        }
-        if(j+1 < c && board[i][j+1] == word.charAt(idx+1) && !vis[i][j+1] && helper(vis,board,word,i,j+1,idx+1)) {
-            return true;
-        }
+        if(helper(vis,board,word,i+1,j,idx+1)) return true;
+        if(helper(vis,board,word,i-1,j,idx+1)) return true;
+        if(helper(vis,board,word,i,j+1,idx+1)) return true;
+        if(helper(vis,board,word,i,j-1,idx+1)) return true;
         vis[i][j] = false;
         return false;
     }
     public boolean exist(char[][] board, String word) {
-        boolean[][] vis = new boolean[board.length][board[0].length];
         for(int i = 0;i < board.length;i++) {
             for(int j = 0;j < board[0].length;j++) {
-                if(board[i][j] == word.charAt(0) && helper(vis,board,word,i,j,0)) {
-                    return true;
+                if(board[i][j] == word.charAt(0)) {
+                    boolean[][] vis = new boolean[board.length][board[0].length];
+                    if(helper(vis,board,word,i,j,0)) return true;
                 }
             }
         }
