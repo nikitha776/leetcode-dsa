6735c6f3292db2d36af954c671269f5dd602b638
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-05-19 12:51:29 +0530

    Time: 3 ms (41.37%) | Memory: 41.6 MB (91.61%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,16 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
-        while (true) {
-            if (s.contains("()")) {
-                s = s.replace("()", "");
-            } else if (s.contains("{}")) {
-                s = s.replace("{}", "");
-            } else if (s.contains("[]")) {
-                s = s.replace("[]", "");
+        Stack<Character> st = new Stack<>();
+        Map<Character,Character> map = new HashMap<>();
+        map.put(']','[');
+        map.put(')','(');
+        map.put('}','{');
+        for(int i = 0;i < s.length();i++) {
+            char ch = s.charAt(i);
+            if(ch == '[' || ch == '{' || ch == '(') {
+                st.push(s.charAt(i));
             } else {
-                // If the string becomes empty, it indicates all brackets are matched.
-                return s.isEmpty();
+                if(st.isEmpty() || map.get(ch) != st.peek()) {
+                    return false;
+                } else {
+                    st.pop();
+                }
             }
         }
+        return st.isEmpty();
     }
 }
