ebecc5f3ee973a0446f4bc588f44d661a2ab7b18
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-05-21 12:49:30 +0530

    Time: 3 ms (90.57%) | Memory: 44.5 MB (25.54%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,20 +1,28 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        Stack<Integer> st=new Stack<>();
-        for(int i=nums2.length-1;i>=0;i--){
-            while(!st.isEmpty() && st.peek()<nums2[i]){
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int[] res = new int[n];
+        for(int i = m-1;i >= 0;i--) {
+            if(i == m-1) {
+                map.put(nums2[i],-1);
+                st.push(nums2[i]);
+                continue;
+            }
+            while(!st.isEmpty() && st.peek() < nums2[i]) {
                 st.pop();
             }
-            if(st.isEmpty()) hm.put(nums2[i],-1);
-            else hm.put(nums2[i],st.peek());
-            st.push(nums2[i]); 
+            if(st.isEmpty()) map.put(nums2[i],-1);
+            else map.put(nums2[i],st.peek());
+            st.push(nums2[i]);
         }
-
-        int [] ans=new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            ans[i]=hm.get(nums1[i]);
+        for(int i= 0;i < n;i++) {
+            res[i] = map.get(nums1[i]);
         }
-        return ans;
+        return res;
     }
 }
