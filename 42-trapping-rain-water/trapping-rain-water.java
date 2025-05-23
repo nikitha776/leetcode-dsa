a986d245498b64be996762088e00ae63544cb755
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-05-23 20:06:00 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (28.59%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,24 +1,40 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        int[] prefMax = new int[n];
-        int[] suffMax = new int[n];
+        int leftMax = 0;
+        int rightMax = 0;
+        int l = 0;
+        int r = n-1;
         int total = 0;
-        prefMax[0] = height[0];
-        suffMax[n-1] = height[n-1];
-
-        for(int i = 1;i < n;i++) {
-            prefMax[i] = Math.max(height[i],prefMax[i-1]);
-        }
-        for(int i = n-2;i >= 0;i--) {
-            suffMax[i] = Math.max(height[i],suffMax[i+1]);
-        }
-
-        for(int i = 0;i < n;i++) {
-            if(height[i] < prefMax[i] && height[i] < suffMax[i]) {
-                total += Math.min(prefMax[i],suffMax[i]) - height[i];
+        while(l < r) {
+            leftMax = Math.max(height[l],leftMax);
+            rightMax = Math.max(height[r],rightMax);
+            if(leftMax < rightMax) {
+                total += leftMax - height[l];
+                l++;
+            } else {
+                total += rightMax - height[r];
+                r--;
             }
         }
+        // int[] prefMax = new int[n];
+        // int[] suffMax = new int[n];
+        // int total = 0;
+        // prefMax[0] = height[0];
+        // suffMax[n-1] = height[n-1];
+
+        // for(int i = 1;i < n;i++) {
+        //     prefMax[i] = Math.max(height[i],prefMax[i-1]);
+        // }
+        // for(int i = n-2;i >= 0;i--) {
+        //     suffMax[i] = Math.max(height[i],suffMax[i+1]);
+        // }
+
+        // for(int i = 0;i < n;i++) {
+        //     if(height[i] < prefMax[i] && height[i] < suffMax[i]) {
+        //         total += Math.min(prefMax[i],suffMax[i]) - height[i];
+        //     }
+        // }
         return total;
     }
 }
