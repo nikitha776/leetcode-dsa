3c9937c9f4e440d055450ee493d9d19de5e217b8
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-05-31 18:54:43 +0530

    Time: 16 ms (79.63%) | Memory: 44.1 MB (96.17%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,33 +1,31 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        // int n = nums.length;
-        // int total = 0;
-        // int max = Integer.MIN_VALUE;
-        // int min = Integer.MAX_VALUE;
-        // for(int i = 0;i < n;i++) {
-        //     min = nums[i];
-        //     max = nums[i];
-        //     for(int j = i;j < n;j++) {
-        //         min = Math.min(min,nums[j]);
-        //         max = Math.max(max,nums[j]);
-        //         total += max-min;
-        //     }
-        // }
-        // return total;
-
-
-        int n=nums.length;
-        long res=0;
-        for(int i=0;i<n;i++){
-            int min=nums[i];
-            int max=nums[i];
-            for(int j=i;j<n;j++){
-                min=Math.min(min,nums[j]);
-                max=Math.max(max,nums[j]);
-                res+=max-min;
+        int n = nums.length;
+        long total = 0;
+        for(int i = 0;i < n;i++) {
+            int min = nums[i];
+            int max = nums[i];
+            for(int j = i;j < n;j++) {
+                min = Math.min(min,nums[j]);
+                max = Math.max(max,nums[j]);
+                total += max-min;
             }
         }
-        return res;
+        return total;
+
+
+        // int n=nums.length;
+        // long res=0;
+        // for(int i=0;i<n;i++){
+        //     int min=nums[i];
+        //     int max=nums[i];
+        //     for(int j=i;j<n;j++){
+        //         min=Math.min(min,nums[j]);
+        //         max=Math.max(max,nums[j]);
+        //         res+=max-min;
+        //     }
+        // }
+        // return res;
     
     }
 }
