71ad4323c5ad56979126bdf99e8f58d075e6d02a
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-05-31 20:30:28 +0530

    Time: 43 ms (18.03%) | Memory: 44.8 MB (29.32%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,31 +1,86 @@
 class Solution {
+    public int[] findNse(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        int[] nse = new int[n];
+        for(int i = n-1;i >= 0;i--) {
+            while(!st.isEmpty() && (nums[st.peek()] >= nums[i])) {
+                st.pop();
+            }
+            nse[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+        return nse;
+    }
+    public int[] findPsee(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        int[] psee = new int[n];
+        for(int i = 0;i < n;i++) {
+            while(!st.isEmpty() && nums[st.peek()] > nums[i]) {
+                st.pop();
+            }
+            psee[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+        return psee;
+    }
+    public int[] findNgee(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        int[] ngee = new int[n];
+        for(int i = n-1;i >= 0;i--) {
+            while(!st.isEmpty() && nums[st.peek()] <= nums[i]) {
+                st.pop();
+            }
+            ngee[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+        return ngee;
+    }
+    public int[] findPge(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        int[] pge = new int[n];
+        for(int i = 0;i < n;i++) {
+            while(!st.isEmpty() && nums[st.peek()] < nums[i]) {
+                st.pop();
+            }
+            pge[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+        return pge;
+    }
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
-        long total = 0;
+        long minTotal = 0L;
+        long maxTotal = 0L;
+        int[] nse = findNse(nums);
+        int[] psee = findPsee(nums);
+        int[] ngee = findNgee(nums);
+        int[] pge = findPge(nums);
         for(int i = 0;i < n;i++) {
-            int min = nums[i];
-            int max = nums[i];
-            for(int j = i;j < n;j++) {
-                min = Math.min(min,nums[j]);
-                max = Math.max(max,nums[j]);
-                total += max-min;
-            }
+            long left = i - psee[i];
+            long right = nse[i] - i;
+            minTotal = minTotal + (left*right*nums[i]);
         }
-        return total;
-
-
-        // int n=nums.length;
-        // long res=0;
-        // for(int i=0;i<n;i++){
-        //     int min=nums[i];
-        //     int max=nums[i];
-        //     for(int j=i;j<n;j++){
-        //         min=Math.min(min,nums[j]);
-        //         max=Math.max(max,nums[j]);
-        //         res+=max-min;
+        for(int i = 0;i < n;i++) {
+            long left = i - pge[i];
+            long right = ngee[i] - i;
+            maxTotal = maxTotal + (left*right*nums[i]);
+        }
+        return maxTotal - minTotal;
+        // int n = nums.length;
+        // long total = 0;
+        // for(int i = 0;i < n;i++) {
+        //     int min = nums[i];
+        //     int max = nums[i];
+        //     for(int j = i;j < n;j++) {
+        //         min = Math.min(min,nums[j]);
+        //         max = Math.max(max,nums[j]);
+        //         total += max-min;
         //     }
         // }
-        // return res;
-    
+        // return total;
     }
 }
