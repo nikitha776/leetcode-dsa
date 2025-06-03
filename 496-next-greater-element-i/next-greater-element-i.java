db6d9db98f9fb6149bc8d0845ee3748c759e1ffc
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-06-03 12:07:21 +0530

    Time: 3 ms (91.90%) | Memory: 44.6 MB (12.88%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,5 +1,30 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // Stack<Integer> st = new Stack<>();
+        // HashMap<Integer,Integer> map = new HashMap<>();
+
+        // int n = nums1.length;
+        // int m = nums2.length;
+
+        // int[] res = new int[n];
+        // for(int i = m-1;i >= 0;i--) {
+        //     if(i == m-1) {
+        //         map.put(nums2[i],-1);
+        //         st.push(nums2[i]);
+        //         continue;
+        //     }
+        //     while(!st.isEmpty() && st.peek() < nums2[i]) {
+        //         st.pop();
+        //     }
+        //     if(st.isEmpty()) map.put(nums2[i],-1);
+        //     else map.put(nums2[i],st.peek());
+        //     st.push(nums2[i]);
+        // }
+        // for(int i= 0;i < n;i++) {
+        //     res[i] = map.get(nums1[i]);
+        // }
+        // return res;
+
         Stack<Integer> st = new Stack<>();
         HashMap<Integer,Integer> map = new HashMap<>();
 
@@ -7,20 +32,18 @@ class Solution {
         int m = nums2.length;
 
         int[] res = new int[n];
-        for(int i = m-1;i >= 0;i--) {
-            if(i == m-1) {
-                map.put(nums2[i],-1);
-                st.push(nums2[i]);
-                continue;
-            }
-            while(!st.isEmpty() && st.peek() < nums2[i]) {
+
+        for(int i = 0;i < m;i++) {
+            while(!st.isEmpty() && nums2[st.peek()] < nums2[i]) {
+                map.put(nums2[st.peek()],nums2[i]);
                 st.pop();
             }
-            if(st.isEmpty()) map.put(nums2[i],-1);
-            else map.put(nums2[i],st.peek());
-            st.push(nums2[i]);
+            st.push(i);
         }
-        for(int i= 0;i < n;i++) {
+        while(!st.isEmpty()) {
+            map.put(nums2[st.pop()],-1);
+        }
+        for(int i = 0;i < n;i++) {
             res[i] = map.get(nums1[i]);
         }
         return res;
