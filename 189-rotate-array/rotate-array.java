5999830642028353a5db084c4d24ebde5cd946c9
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-06-05 12:46:53 +0530

    Time: 2 ms (27.80%) | Memory: 57.5 MB (36.06%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -2,29 +2,29 @@ class Solution {
     public void rotate(int[] nums, int k) {
         k = k % nums.length;
         if(nums.length == 1) return;
-        int n= nums.length;
-        int[] temp = new int[k];
-        for(int i = 0;i < k;i++) {
-            temp[i] = nums[n-k+i];
-        }
-        for(int i = n-1;i >= k;i--) {
-            nums[i] = nums[i-k];
-        }
-        for(int i = 0;i < k;i++) {
-            nums[i] = temp[i];
-        }
+        // int n= nums.length;
+        // int[] temp = new int[k];
+        // for(int i = 0;i < k;i++) {
+        //     temp[i] = nums[n-k+i];
+        // }
+        // for(int i = n-1;i >= k;i--) {
+        //     nums[i] = nums[i-k];
+        // }
+        // for(int i = 0;i < k;i++) {
+        //     nums[i] = temp[i];
+        // }
         
-        // reverse(nums,0,nums.length-1);
-        // reverse(nums,0,k-1);
-        // reverse(nums,k,nums.length-1);
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
     }
-    // public static void reverse(int[] nums,int s,int e) {
-    //     while(s < e) {
-    //         int temp = nums[s];
-    //         nums[s] = nums[e];
-    //         nums[e] = temp;
-    //         s++;
-    //         e--;
-    //     }
-    // }
+    public static void reverse(int[] nums,int s,int e) {
+        while(s < e) {
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
+            s++;
+            e--;
+        }
+    }
 }
