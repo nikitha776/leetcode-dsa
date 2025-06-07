786a8ae144c93c2074dac6c2237681ddc3f653e8
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-06-07 10:55:01 +0530

    Time: 1 ms (93.01%) | Memory: 45.3 MB (88.62%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -18,17 +18,29 @@ class Solution {
         // }
         // return -1;
 
+        // int n = nums.length;
+        // int[] suffSum = new int[n];
+        // int sum = 0;
+        // for(int i = n-1;i >= 0;i--) {
+        //     sum += nums[i];
+        //     suffSum[i] = sum;
+        // }
+        // sum = 0;
+        // for(int i = 0;i < n;i++) {
+        //     sum += nums[i];
+        //     if(sum == suffSum[i]) return i;
+        // }
+        // return -1;
+
         int n = nums.length;
-        int[] suffSum = new int[n];
-        int sum = 0;
-        for(int i = n-1;i >= 0;i--) {
-            sum += nums[i];
-            suffSum[i] = sum;
-        }
-        sum = 0;
+        int total = 0;
         for(int i = 0;i < n;i++) {
-            sum += nums[i];
-            if(sum == suffSum[i]) return i;
+            total += nums[i];
+        }
+        int leftSum = 0;
+        for(int i = 0;i < n;i++) {
+            if(leftSum == (total-leftSum-nums[i])) return i;
+            leftSum += nums[i];
         }
         return -1;
     }
