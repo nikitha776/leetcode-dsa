ffe60e63430b5760bc4ee7df293452adb4205050
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-06-07 10:48:01 +0530

    Time: 1 ms (93.01%) | Memory: 46.1 MB (8.57%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,20 +1,34 @@
 class Solution {
     public int pivotIndex(int[] nums) {
+        // int n = nums.length;
+        // int[] prefSum = new int[n];
+        // int sum = 0;
+        // for(int i = 0;i < n;i++) {
+        //     sum += nums[i];
+        //     prefSum[i] = sum;
+        // }
+        // int[] suffSum = new int[n];
+        // sum = 0;
+        // for(int i = n-1;i >= 0;i--) {
+        //     sum += nums[i];
+        //     suffSum[i] = sum;
+        // }
+        // for(int i = 0;i < n;i++) {
+        //     if(prefSum[i] == suffSum[i]) return i;
+        // }
+        // return -1;
+
         int n = nums.length;
-        int[] prefSum = new int[n];
-        int sum = 0;
-        for(int i = 0;i < n;i++) {
-            sum += nums[i];
-            prefSum[i] = sum;
-        }
         int[] suffSum = new int[n];
-        sum = 0;
+        int sum = 0;
         for(int i = n-1;i >= 0;i--) {
             sum += nums[i];
             suffSum[i] = sum;
         }
+        sum = 0;
         for(int i = 0;i < n;i++) {
-            if(prefSum[i] == suffSum[i]) return i;
+            sum += nums[i];
+            if(sum == suffSum[i]) return i;
         }
         return -1;
     }
