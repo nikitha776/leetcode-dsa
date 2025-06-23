0fed569732b9bc3a3dc74886ad27c58bbf883262
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-06-23 13:43:10 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (89.03%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,15 +1,39 @@
- class Solution {
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int idx1 = m-1;
+//         int idx2 = n-1;
+//         int idx = m+n-1;
+
+//         while(idx1 >= 0 && idx2 >= 0) {
+//             if(nums1[idx1] > nums2[idx2]) {
+//                 nums1[idx--] = nums1[idx1--];
+//             } else {
+//                 nums1[idx--] = nums2[idx2--];
+//             }
+//         }
+//         while(idx2 >= 0) {
+//             nums1[idx] = nums2[idx2--];
+//         }
+//     }
+// }
+
+class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-        
-        while (j >= 0) {
-            if (i >= 0 && nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
+        int idx1 = m - 1;
+        int idx2 = n - 1;
+        int idx = m + n - 1;
+
+        while (idx1 >= 0 && idx2 >= 0) {
+            if (nums1[idx1] > nums2[idx2]) {
+                nums1[idx--] = nums1[idx1--];
             } else {
-                nums1[k--] = nums2[j--];
+                nums1[idx--] = nums2[idx2--];
             }
+        }
+
+        // If any elements remain in nums2, copy them
+        while (idx2 >= 0) {
+            nums1[idx--] = nums2[idx2--];
         }
     }
 }
