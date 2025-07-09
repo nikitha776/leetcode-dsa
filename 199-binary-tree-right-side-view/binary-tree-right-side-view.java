ee174792dc1d0ed01f16e4642cd9125f01f3a671
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-07-09 12:46:09 +0530

    Time: 3 ms (2.97%) | Memory: 42.1 MB (62.36%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ class Pair<u, v> {
+    public u first;
+    public v second;
+
+    public Pair(u first, v second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public u getKey() {
+        return first;
+    }
+
+    public v getValue() {
+        return second;
+    }
+}
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        Map<Integer,Integer> map = new TreeMap<>();
+        Queue<Pair<TreeNode,Integer>> q = new LinkedList<>();
+        q.add(new Pair<>(root,0));
+
+        while(!q.isEmpty()) {
+            int n = q.size();
+            for(int i = 0;i < n;i++) {
+                Pair<TreeNode,Integer> p = q.poll();
+                TreeNode temp = p.getKey();
+                int lvl = p.getValue();
+                int val = temp.val;
+                map.put(lvl,val);
+                if(temp.left != null) q.add(new Pair<>(temp.left,lvl+1));
+                if(temp.right != null) q.add(new Pair<>(temp.right,lvl+1));
+            }
+        }
+
+        for(int val : map.values()) {
+            list.add(val);
+        }
+
+        return list;
+    }
+}
