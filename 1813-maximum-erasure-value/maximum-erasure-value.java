d1d8e1bb97a6b73f86b7bdede50bf367c48c99b3
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-07-22 14:36:47 +0530

    Time: 52 ms (45.18%) | Memory: 61.3 MB (24.07%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.java
+++ 1813-maximum-erasure-value/maximum-erasure-value.java
@@ -5,16 +5,15 @@ class Solution {
         int r = 0;
         int n = nums.length;
         int sum = 0;
-        Map<Integer,Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
         while(r < n) {
             sum += nums[r];
-            while(map.containsKey(nums[r])) {
+            while(set.contains(nums[r])) {
                 sum -= nums[l];
-                map.put(nums[l],map.get(nums[l])-1);
-                if(map.get(nums[l]) == 0) map.remove(nums[l]);
+                set.remove(nums[l]);
                 l++;
             }
-            map.put(nums[r],1);
+            set.add(nums[r]);
             maxSum = Math.max(sum,maxSum);
             r++;
         }
