f05f450754cc1761543c413bb3a0784eb02ec833
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-10-18 14:39:42 +0530

    Time: 18 ms (12.89%) | Memory: 47.3 MB (55.99%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -5,42 +5,43 @@ class Pair {
     Pair(int r,int c,int s) {
         row = r;
         col = c;
-        steps= s;
+        steps = s;
     }
 }
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
-        int[][] res = new int[n][m];
         boolean[][] vis = new boolean[n][m];
+        int[][] dist = new int[n][m];
         Queue<Pair> q = new LinkedList<>();
         for(int i = 0;i < n;i++) {
             for(int j = 0;j < m;j++) {
                 if(mat[i][j] == 0) {
                     q.add(new Pair(i,j,0));
-                    vis[i][j] = true;
                 }
             }
         }
         while(!q.isEmpty()) {
-            int size = q.size();
-            for(int i = 0;i < size;i++) {
-                Pair t = q.poll();
-                int r = t.row;
-                int c = t.col;
-                int steps = t.steps;
-                int[] row = {-1,1,0,0};
-                int[] col = {0,0,1,-1}; 
-                for(int j = 0;j < 4;j++) {
-                    if(r+row[j] >= 0 && c+col[j] >= 0 && r+row[j] < n && c+col[j] < m && mat[r+row[j]][c+col[j]] == 1 && !vis[r+row[j]][c+col[j]]) {
-                        vis[r+row[j]][c+col[j]] = true;
-                        res[r+row[j]][c+col[j]] = steps+1;
-                        q.add(new Pair(r+row[j],c+col[j],steps+1));
-                    }
+            int len = q.size();
+            for(int j = 0;j < len;j++) {
+                Pair p = q.poll();
+                int row = p.row;
+                int col = p.col;
+                int steps = p.steps;
+                int[] r = {1,-1,0,0};
+                int[] c = {0,0,1,-1};
+                for(int i = 0;i < 4;i++) {
+                    int ridx = r[i]+row;
+                    int cidx = c[i]+col;
+                    if(ridx >= 0 && ridx < n && cidx >= 0 && cidx < m && mat[ridx][cidx] == 1 && !vis[ridx][cidx]) {
+                        vis[ridx][cidx] = true;
+                        dist[ridx][cidx] = steps+1;
+                        q.add(new Pair(ridx,cidx,steps+1));
+                    } 
                 }
             }
-        } 
-        return res;
+        }
+        return dist;
     }
 }
