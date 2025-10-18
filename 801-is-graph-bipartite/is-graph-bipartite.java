11bde4b891ddce1995d0b59087f7ea08fa97c0d0
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-10-18 18:57:42 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (7.98%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,23 +1,24 @@
 class Solution {
-    public boolean dfs(int[][] graph,int curr,int color,int[] vis) {
-        vis[curr] = color;
-        for(int i : graph[curr]) {
-            if(vis[i] == -1) {
-                int newColor = color == 0 ? 1 : 0;
-                if(!dfs(graph,i,newColor,vis)) return false;
-            } else if(vis[i] == color) return false;
+    public boolean dfs(int[][] graph,int[] vis,int i,int color) {
+        vis[i] = color;
+        int nextColor = color == 0 ? 1 : 0;
+        for(int j : graph[i]) {
+            if(vis[j] == -1) {
+                if(!dfs(graph,vis,j,nextColor)) return false;
+            } else if(vis[j] == color) return false;
         }
         return true;
     }
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
+        int m = graph[0].length;
         int[] vis = new int[n];
         Arrays.fill(vis,-1);
         for(int i = 0;i < n;i++) {
             if(vis[i] == -1) {
-                if(!dfs(graph,i,0,vis)) return false;
+                if(!dfs(graph,vis,i,0)) return false;
             }
         }
-        return dfs(graph,0,0,vis);
+        return true;
     }
 }
