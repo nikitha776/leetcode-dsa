fd28f0f88195069eaaad7873f5618025a8af0409
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-10-22 11:49:26 +0530

    Time: 5 ms (14.09%) | Memory: 43 MB (95.24%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,42 +1,20 @@
 class Solution {
-    // public void helper(int[] nums,List<List<Integer>> lists,List<Integer> list,int sum,int target,int idx) {
-    //     if(sum == target) {
-    //         lists.add(new ArrayList<>(list));
-    //         return;
-    //     }
-    //     if(idx == nums.length || sum > target) {
-    //         return;
-    //     }
-    //     list.add(nums[idx]);
-    //     sum = sum + nums[idx];
-    //     helper(nums,lists,list,sum,target,idx+1);
-    //     while(idx+1 < nums.length && nums[idx] == nums[idx+1]) {
-    //         idx++;
-    //     }
-    //     list.remove(list.size()-1);
-    //     sum = sum - nums[idx];
-    //     helper(nums,lists,list,sum,target,idx+1);
-    //     return;
-    // }
-    public void helper(int[] nums,List<Integer> list,List<List<Integer>> lists,int target,int idx) {
+    public void helper(int[] nums,int target,List<List<Integer>> res,List<Integer> list,int idx) {
         if(target == 0) {
-            lists.add(new ArrayList<>(list));
+            res.add(new ArrayList<>(list));
             return;
         }
-        for(int i = idx;i < nums.length;i++) {
-            if(i > idx && nums[i] == nums[i-1]) continue;
-            if(target < nums[i]) break;
-            list.add(nums[i]);
-            helper(nums,list,lists,target-nums[i],i+1);
-            list.remove(list.size()-1);
-        }
+        if(target < 0 || idx == nums.length) return;
+        list.add(nums[idx]);
+        helper(nums,target-nums[idx],res,list,idx+1);
+        list.remove(list.size()-1);
+        while(idx < nums.length-1 && nums[idx] == nums[idx+1]) idx++;
+        helper(nums,target,res,list,idx+1);
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        List<List<Integer>> lists = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        // helper(candidates,lists,list,0,target,0);
-        helper(candidates,list,lists,target,0);
-        return lists;
+        List<List<Integer>> res = new ArrayList<>();
+        helper(candidates,target,res,new ArrayList<>(),0);
+        return res;
     }
 }
