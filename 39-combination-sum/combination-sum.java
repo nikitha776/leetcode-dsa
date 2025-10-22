7a210fb19ac003242835c8b29a17ca1965c8275b
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2025-10-22 11:26:29 +0530

    Time: 2 ms (84.58%) | Memory: 44.6 MB (82.70%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,37 +1,21 @@
 class Solution {
-    // public void helper(List<List<Integer>> lists,int[] nums,List<Integer> list,int sum,int target,int idx) {
-    //     if(sum == target) {
-    //        lists.add(new ArrayList<>(list));
-    //        return;
-    //     }
-    //     if(sum > target || idx == nums.length);
-    //     list.add(nums[idx]);
-    //     sum = sum + nums[idx];
-    //     helper(lists,nums,list,sum,target,idx);
-    //     list.remove(list.size()-1);
-    //     sum = sum - nums[idx];
-    //     helper(lists,nums,list,sum,target,idx+1);
-    //     return;
-    // }
-    public void helper(List<List<Integer>> lists,int[] nums,List<Integer> list,int target,int idx) {
-        if(idx == nums.length) {
-            if(target == 0) {
-                lists.add(new ArrayList<>(list));
-            }
+    public void helper(int[] nums, int target, int idx, List<List<Integer>> res, List<Integer> list, int n) {
+        if(target == 0) {
+            res.add(new ArrayList<>(list));
             return;
         }
-        if(nums[idx] <= target) {
-            list.add(nums[idx]);
-            helper(lists,nums,list,target-nums[idx],idx);
-            list.remove(list.size()-1);
+        if(idx == n ) {
+            return;
         }
-        helper(lists,nums,list,target,idx+1);
+        if(target < 0) return;
+        list.add(nums[idx]);
+        helper(nums,target-nums[idx],idx,res,list,n);
+        list.remove(list.size()-1);
+        helper(nums,target,idx+1,res,list,n);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> lists = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        // helper(lists,candidates,list,0,target,0);
-        helper(lists,candidates,list,target,0);
-        return lists;
+        List<List<Integer>> res = new ArrayList<>();
+        helper(candidates,target,0,res,new ArrayList<>(),candidates.length);
+        return res;
     }
 }
