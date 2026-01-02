284abb60995c7fb9eca0767ed07dfb8db03d0a46
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2026-01-02 14:54:29 +0530

    Time: 5 ms (25.06%) | Memory: 50.4 MB (34.75%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -10,22 +10,39 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] dp = new int[m][n];
+        // int[][] dp = new int[m][n];
+        // for(int i = 0;i < m;i++) {
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // for(int i = 0;i < m;i++) {
+        //     for(int j = 0;j < n;j++) {
+        //         if(i == 0 && j == 0) {
+        //             dp[i][j] = grid[0][0];
+        //             continue;
+        //         }
+        //         int up = (i > 0) ? grid[i][j] + dp[i-1][j] : (int) 1e9;
+        //         int left = (j > 0) ? grid[i][j] + dp[i][j-1] : (int) 1e9;
+        //         dp[i][j] = Math.min(up,left);
+        //     }
+        // }
+        // return dp[m-1][n-1];
+        // return helper(grid,dp,m-1,n-1);
+
+        int[] prev = new int[n];
+
         for(int i = 0;i < m;i++) {
-            Arrays.fill(dp[i],-1);
-        }
-        for(int i = 0;i < m;i++) {
+            int[] temp = new int[n];
             for(int j = 0;j < n;j++) {
                 if(i == 0 && j == 0) {
-                    dp[i][j] = grid[0][0];
+                    temp[j] = grid[i][j];
                     continue;
                 }
-                int up = (i > 0) ? grid[i][j] + dp[i-1][j] : (int) 1e9;
-                int left = (j > 0) ? grid[i][j] + dp[i][j-1] : (int) 1e9;
-                dp[i][j] = Math.min(up,left);
+                int up = (i > 0) ? grid[i][j] + prev[j] : (int) 1e9;
+                int left = (j > 0) ? grid[i][j] + temp[j-1] : (int) 1e9;
+                temp[j] = Math.min(up,left);
             }
+            prev = temp;
         }
-        return dp[m-1][n-1];
-        // return helper(grid,dp,m-1,n-1);
+        return prev[n-1];
     }
 }
