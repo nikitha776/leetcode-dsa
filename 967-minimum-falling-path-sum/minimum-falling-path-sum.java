8621c51ee47f441aa7c0237a4e97f553b23421c2
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2026-01-02 18:38:33 +0530

    Time: 7 ms (54.53%) | Memory: 47.5 MB (9.55%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -11,32 +11,57 @@ class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
         int min = Integer.MAX_VALUE;
-        int[][] dp = new int[n][n];
-        for(int i = 0;i < n;i++) {
-            Arrays.fill(dp[i],-1);
-        }
+        // int[][] dp = new int[n][n];
+        // for(int i = 0;i < n;i++) {
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // // for(int j = 0;j < n;j++) {
+        // //     min = Math.min(min,helper(matrix,dp,0,j));
+        // // }
+        // // return min;
+
         // for(int j = 0;j < n;j++) {
-        //     min = Math.min(min,helper(matrix,dp,0,j));
+        //     dp[n-1][j] = matrix[n-1][j];
+        // }
+        
+        // for(int i = n-2;i >= 0;i--) {
+        //     for(int j = n-1;j >= 0;j--) {
+        //         int sum1 = Integer.MAX_VALUE;
+        //         int sum2 = Integer.MAX_VALUE;
+        //         int sum3 = Integer.MAX_VALUE;
+        //         if(j > 0) sum1 = matrix[i][j] + dp[i+1][j-1];
+        //         sum2 = matrix[i][j] + dp[i+1][j];
+        //         if(j < n-1) sum3 = matrix[i][j] + dp[i+1][j+1];
+        //         dp[i][j] = Math.min(sum1,Math.min(sum2,sum3));
+        //     }
+        // }
+
+        // for(int i = 0;i < n;i++) {
+        //     min = Math.min(min,dp[0][i]);
         // }
         // return min;
+
+        int[] next = new int[n];
+
         for(int j = 0;j < n;j++) {
-            dp[n-1][j] = matrix[n-1][j];
+            next[j] = matrix[n-1][j];
         }
-        
+
         for(int i = n-2;i >= 0;i--) {
+            int[] temp = new int[n];
             for(int j = n-1;j >= 0;j--) {
                 int sum1 = Integer.MAX_VALUE;
                 int sum2 = Integer.MAX_VALUE;
                 int sum3 = Integer.MAX_VALUE;
-                if(j > 0) sum1 = matrix[i][j] + dp[i+1][j-1];
-                sum2 = matrix[i][j] + dp[i+1][j];
-                if(j < n-1) sum3 = matrix[i][j] + dp[i+1][j+1];
-                dp[i][j] = Math.min(sum1,Math.min(sum2,sum3));
+                if(j > 0) sum1 = matrix[i][j] + next[j-1];
+                sum2 = matrix[i][j] + next[j];
+                if(j < n-1) sum3 = matrix[i][j] + next[j+1];
+                temp[j] = Math.min(sum1,Math.min(sum2,sum3));
             }
+            next = temp;
         }
-
         for(int i = 0;i < n;i++) {
-            min = Math.min(min,dp[0][i]);
+            min = Math.min(min,next[i]);
         }
         return min;
     }
