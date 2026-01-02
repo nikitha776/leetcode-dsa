cfc88794ef7b333c8f2cfc7a5352be29d8e01bff
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2026-01-02 14:36:40 +0530

    Time: 1 ms (17.89%) | Memory: 43.8 MB (9.84%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -12,26 +12,47 @@ class Solution {
         int n = obstacleGrid[0].length;
         if(obstacleGrid[0][0] == 1) return 0;
 
-        int[][] dp = new int[m][n];
-        for(int i = 0;i < m;i++) {
-            Arrays.fill(dp[i],-1);
-        }
+        // int[][] dp = new int[m][n];
+        // for(int i = 0;i < m;i++) {
+        //     Arrays.fill(dp[i],-1);
+        // }
         // return helper(obstacleGrid,m-1,n-1,dp);
 
+        // for(int i = 0;i < m;i++) {
+        //     for(int j = 0;j < n;j++) {
+        //         if(i == 0 && j == 0) {
+        //             dp[i][j] = 1;
+        //             continue;
+        //         }
+        //         int up = 0;
+        //         int left = 0;
+        //         if(i > 0) up = dp[i-1][j];
+        //         if(j > 0) left = dp[i][j-1];
+        //         dp[i][j] = up + left;
+        //         if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+        int[] prev = new int[m];
+        prev[0] = 1;
+
         for(int i = 0;i < m;i++) {
+            int[] temp = new int[n];
             for(int j = 0;j < n;j++) {
                 if(i == 0 && j == 0) {
-                    dp[i][j] = 1;
+                    temp[0] = 1;
                     continue;
                 }
                 int up = 0;
                 int left = 0;
-                if(i > 0) up = dp[i-1][j];
-                if(j > 0) left = dp[i][j-1];
-                dp[i][j] = up + left;
-                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                if(i > 0) up = prev[j];
+                if(j > 0) left = temp[j-1];
+                temp[j] = up + left;
+                if(obstacleGrid[i][j] == 1) temp[j] = 0;
             }
+            prev = temp;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 }
