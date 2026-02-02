9fe20d221eab8abc56ba0884f760f42c9ec84d8d
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2026-02-02 16:49:56 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (77.86%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,50 +1,17 @@
 class Solution {
-    public int helper(int i,int j,int[][] dp) {
-        if(i == 0 && j == 0) return 1;
-        if(i < 0 || j < 0) return 0;
+    public int helper(int m,int n,int i,int j,int[][] dp) {
+        if(i == 0 && j == 0) return dp[i][j] = 1;
+        if(i < 0 || j < 0 || i > m-1 || j > n-1) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        int up = helper(i-1,j,dp);
-        int left = helper(i,j-1,dp);
-        return dp[i][j] = up + left;
+        int top = helper(m,n,i-1,j,dp);
+        int left = helper(m,n,i,j-1,dp);
+        return dp[i][j] = top+left;
     }
     public int uniquePaths(int m, int n) {
-        // int[][] dp = new int[m][n];
-        // for(int i = 0;i < m;i++) {
-        //     Arrays.fill(dp[i],-1);
-        // }
-        // // return helper(m-1,n-1,dp);
-        // dp[0][0] = 1;
-        // for(int i = 0;i < m;i++) {
-        //     for(int j = 0;j < n;j++) {
-        //         if(i == 0 && j == 0) continue;
-        //         int up = 0;
-        //         int left = 0;
-        //         if(i > 0) up = dp[i-1][j];
-        //         if(j > 0) left = dp[i][j-1];
-        //         dp[i][j] = up + left;
-        //     }
-        // }
-        // return dp[m-1][n-1];
-
-        int[] prev = new int[n];
-
-        prev[0] = 1;
-
+        int[][] dp = new int[m][n];
         for(int i = 0;i < m;i++) {
-            int[] temp = new int[n];
-            for(int j = 0;j < n;j++) {
-                if(i == 0 && j == 0) {
-                    temp[j] = 1;
-                    continue;
-                }
-                int up = 0;
-                int left = 0;
-                if(i > 0) up = prev[j];
-                if(j > 0) left = temp[j-1];
-                temp[j] = up + left;
-            }
-            prev = temp;
+            Arrays.fill(dp[i],-1);
         }
-        return prev[n-1];
+        return helper(m,n,m-1,n-1,dp);
     }
 }
