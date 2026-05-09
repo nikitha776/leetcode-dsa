29fb2023f6af4deabfa416e27bb5ada9f6212399
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2026-05-09 11:17:37 +0530

    Time: 4 ms (75.05%) | Memory: 45.2 MB (48.98%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,49 +1,19 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        // Stack<Integer> st = new Stack<>();
-        // HashMap<Integer,Integer> map = new HashMap<>();
-
-        // int n = nums1.length;
-        // int m = nums2.length;
-
-        // int[] res = new int[n];
-        // for(int i = m-1;i >= 0;i--) {
-        //     if(i == m-1) {
-        //         map.put(nums2[i],-1);
-        //         st.push(nums2[i]);
-        //         continue;
-        //     }
-        //     while(!st.isEmpty() && st.peek() < nums2[i]) {
-        //         st.pop();
-        //     }
-        //     if(st.isEmpty()) map.put(nums2[i],-1);
-        //     else map.put(nums2[i],st.peek());
-        //     st.push(nums2[i]);
-        // }
-        // for(int i= 0;i < n;i++) {
-        //     res[i] = map.get(nums1[i]);
-        // }
-        // return res;
-
         Stack<Integer> st = new Stack<>();
         HashMap<Integer,Integer> map = new HashMap<>();
-
-        int n = nums1.length;
-        int m = nums2.length;
-
-        int[] res = new int[n];
-
-        for(int i = 0;i < m;i++) {
-            while(!st.isEmpty() && nums2[st.peek()] < nums2[i]) {
-                map.put(nums2[st.peek()],nums2[i]);
+        int n = nums2.length;
+        for(int i = n-1;i >= 0;i--) {
+            while(!st.isEmpty() && st.peek() <= nums2[i]) {
                 st.pop();
             }
-            st.push(i);
+            if(!st.isEmpty()) map.put(nums2[i],st.peek());
+            else map.put(nums2[i],-1);
+            st.push(nums2[i]);
         }
-        while(!st.isEmpty()) {
-            map.put(nums2[st.pop()],-1);
-        }
-        for(int i = 0;i < n;i++) {
+        int m = nums1.length;
+        int[] res = new int[m];
+        for(int i = 0;i < m;i++) {
             res[i] = map.get(nums1[i]);
         }
         return res;
