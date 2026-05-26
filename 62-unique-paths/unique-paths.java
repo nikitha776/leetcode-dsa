1dd0bf3335dd9d565e90f579e932eec5f9a4f2f8
Vissamsetty Nikitha <vlnikitha2006@gmail.com>
2026-05-26 12:59:52 +0530

    Time: 1 ms (13.62%) | Memory: 42 MB (80.18%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -9,9 +9,21 @@ class Solution {
     }
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
+        // for(int i = 0;i < m;i++) {
+        //     Arrays.fill(dp[i],-1);
+        // }
+        // return helper(m,n,m-1,n-1,dp);
+        dp[0][0] = 1;
         for(int i = 0;i < m;i++) {
-            Arrays.fill(dp[i],-1);
+            for(int j = 0;j < n;j++) {
+                if(i == 0 && j == 0) continue;
+                int top = 0;
+                if(i > 0) top = dp[i-1][j];
+                int left = 0;
+                if(j > 0) left = dp[i][j-1];
+                dp[i][j] = top + left;
+            }
         }
-        return helper(m,n,m-1,n-1,dp);
+        return dp[m-1][n-1];
     }
 }
